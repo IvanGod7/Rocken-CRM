0b9e18a2172fd100a247eb083b796c82bd982bc5
IvanGod7 <igodonuk@gmail.com>
2022-10-06 00:59:30 +0300

    added Anstellungsart object

---
+++ Object Repository/Sandbox_CRM/Page_Anstellungsart/h3_Neu Anstellungsart.rs
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Neu Anstellungsart</name>
+   <tag></tag>
+   <elementGuidId>e9ac5fac-18c2-41df-bd6d-850f51b27d64</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h3.box-title.card-view</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='p0']/div/div/div/div/h3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>61211cf7-10f4-41a6-bb1e-4bd15836653d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>box-title card-view</value>
+      <webElementGuid>ab49869a-56a3-4dde-9dae-5c1b92c36634</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                            Neu Anstellungsart                        </value>
+      <webElementGuid>edef49f9-e251-4190-8d3a-a21925a6b947</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;p0&quot;)/div[@class=&quot;row&quot;]/div[@class=&quot;col-sm-6&quot;]/div[@class=&quot;box&quot;]/div[@class=&quot;box-header with-border&quot;]/h3[@class=&quot;box-title card-view&quot;]</value>
+      <webElementGuid>2e4fc830-9ef4-487a-8517-8b4ab36db872</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='p0']/div/div/div/div/h3</value>
+      <webElementGuid>4fdd807c-b768-471f-bb40-fe2ccef8d7fd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kantone'])[2]/following::h3[1]</value>
+      <webElementGuid>278501c2-e1c1-477d-99fc-518319b882df</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Positionen'])[2]/following::h3[1]</value>
+      <webElementGuid>871857d3-8f01-429e-aef0-5ffb4ec82637</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Name'])[1]/preceding::h3[1]</value>
+      <webElementGuid>a5661409-7df7-4376-acff-6afa96774648</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='SPEICHERN'])[1]/preceding::h3[1]</value>
+      <webElementGuid>793ab9e6-2c82-45ae-81a4-e2656df6b813</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Neu Anstellungsart']/parent::*</value>
+      <webElementGuid>cea5eb8e-fa85-457e-a0d9-f387b0560c15</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h3</value>
+      <webElementGuid>ddc7a237-51fa-4709-9749-f956ef3fb2ea</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = '
+                            Neu Anstellungsart                        ' or . = '
+                            Neu Anstellungsart                        ')]</value>
+      <webElementGuid>2db6fec7-e053-47bc-bf64-cfc7ee13c114</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
